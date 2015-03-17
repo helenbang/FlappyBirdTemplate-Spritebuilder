95254e01672d60f6cb0866984929277cba4cc8dc
Helen Bang <helenbang@Helens-MacBook-Pro.local>
2015-03-17 19:05:32 +0900

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.h
+++ FlappyBird.spritebuilder/Source/GamePlayScene.h
@@ -18,6 +18,7 @@ typedef NS_ENUM(NSInteger, DrawingOrder) {
 @interface GamePlayScene : CCNode <CCPhysicsCollisionDelegate>
 {
     // define variables here;
+    float timeSinceObstacle;
     Character* character;
     CCPhysicsNode* physicsNode; // reference a node that simulates physics
     // node: single unit in a hierarchy
--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -11,12 +11,28 @@
     // load a new Character from the CCB file we created for you named Character
     // CCB files are created by SpriteBuilder and describe the properties and appearance of in-game objects like the bird
     [physicsNode addChild:character];
-    // [ObjectName methodName: parameter];
+    
+    [self addObstacle];
+    
+    timeSinceObstacle = 0.0f;
+    // games typically use "update loop" or "run loop" for regularly scheduled activities (runs really fast)
 }
 
 -(void)update:(CCTime)delta
 {
+    // run every fram
+    // delta is the time that has elapsed since the last time it has run
+    // Cocos2D = this method will get called 60 times a second
+    
     // put update code here
+    // Increment the time since the last obstacle was added
+    timeSinceObstacle += delta; // delta is approximately 1/60th of a second
+    
+    // Check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f){
+        [self addObstacle]; // add a new obstacle
+        timeSinceObstacle = 0.0f; // then reset the timer
+    }
 }
 
 // put new methods here
@@ -25,7 +41,7 @@
     [character flap];
     // calls the flap method
     
-    [self addObstacle];
+    
 }
 
 @end
